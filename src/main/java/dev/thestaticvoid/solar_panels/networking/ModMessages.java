a27fbf1e71cde7ffa924daf216f29f94f3bfdce7
Evelyn3440 <82115237+Evelyn3440@users.noreply.github.com>
2025-01-16 16:06:34 -0500

    FRICK FORGOT AN IMPORT

--- src/main/java/dev/thestaticvoid/solar_panels/networking/ModMessages.java
+++ src/main/java/dev/thestaticvoid/solar_panels/networking/ModMessages.java
@@ -3,6 +3,8 @@ package dev.thestaticvoid.solar_panels.networking;
 import dev.thestaticvoid.solar_panels.SolarPanels;
 import dev.thestaticvoid.solar_panels.util.ResourceIdentifier;
 import net.fabricmc.fabric.api.client.networking.v1.ClientPlayNetworking;
+import net.fabricmc.loader.api.FabricLoader;
+import net.fabricmc.api.EnvType;
 
 public class ModMessages {
     public static final ResourceIdentifier ENERGY_AMOUNT_SYNC = new ResourceIdentifier("energy_amount_sync");
